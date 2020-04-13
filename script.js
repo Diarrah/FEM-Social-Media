e7b8d03f0a0481e291970869e5e7a341ed4e83fc
Diarrah <diarrahcasiano@gmail.com>
2020-04-12 20:48:45 -0700

    Added dynamically changable label text for light mode

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let darkMode = document.getElementById('dark-mode');
+let theme = document.getElementById('theme')
 let { body } = document;
 
 darkMode.addEventListener('change', (e) => {
     darkMode.checked ? body.className = 'dark' : body.className = 'light';
+
+    if (darkMode.checked) {
+        theme.innerText = `Light Mode`
+    } else { theme.innerText = `Dark Mode` }
 });
